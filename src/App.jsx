e972bf19faad5d3f9294008457539c52f8ae1f6e
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-10 07:45:24 +0100

    Update update setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from "pages/Home";
+import Home from 'pages/Home';
 
 const App = () => (
   <>
-    <Navbar />
     <div className="content">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   </>
