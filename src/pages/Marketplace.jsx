5ec181416743ea22bc5887ebe75b82f22acd0e4c
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-10 11:38:49 +0100

    Create page components

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from 'components/Header';
 
-const Home = () => {
-  const message = 'Home page';
+const MarketPlace = () => {
+  const message = 'Marketplace page';
   return (
     <>
       <Header />
@@ -11,4 +11,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default MarketPlace;
