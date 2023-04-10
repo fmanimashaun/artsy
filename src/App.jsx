99e5211d53dfcd90bd3844b3d852102ac22ac00b
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-10 11:38:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
+import Auction from 'pages/Auction';
+import Drop from 'pages/Drop';
+import Marketplace from 'pages/Marketplace';
 
 const App = () => (
   <>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/marketplace" element={<Marketplace />} />
+      <Route path="/auction" element={<Auction />} />
+      <Route path="/drop" element={<Drop />} />
     </Routes>
   </>
 );
